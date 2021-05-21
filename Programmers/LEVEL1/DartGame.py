d9ea6940307f72d1f5b3648c1ffd54fb11fb3d3f
JaeBin <jaebinlee96@kookmin.ac.kr>
2021-05-21 18:20:42 +0900

    Solve this problem not by myself. Divide cases correctly.

--- Programmers/LEVEL1/DartGame.py
+++ Programmers/LEVEL1/DartGame.py
@@ -4,31 +4,31 @@
 def solution(dartResult):
     answer = 0
 
-    bonus = {'S' : 1, 'D' : 2, 'T' : 3}
-    option = {'*' : 2, '#' : -1}
+    bonus = {'S': 1, 'D': 2, 'T': 3}
+    option = {'*': 2, '#': -1}
 
-    Third_period = [0, 0, 0]
-    flag = -1
-
+    chances = [0, 0, 0]
+    number_cnt = -1
     for idx, dart in enumerate(dartResult):
         if dart.isdigit():
-            flag += 1
+            number_cnt += 1
             if dart == '0':
                 continue
-            elif dartResult[idx + 1].isdigit():  # 10 이상일 때
-                Third_period[flag] = int(dart) * 10
-                flag -= 1
+            # 10일 때
+            elif dartResult[idx + 1].isdigit():
+                chances[number_cnt] = int(dart) * 10
+                number_cnt -= 1
             else:
-                Third_period[flag] = int(dart)
-        elif dart in 'SDT':  # SDT
-            Third_period[flag] **= bonus[dart]
+                chances[number_cnt] = int(dart)
+        elif dart in 'SDT':
+            chances[number_cnt] **= bonus[dart]
         else:
             if dart == '*':
-                Third_period[flag - 1] *= 2
+                chances[number_cnt - 1] *= 2
+            else:
+                chances[number_cnt - 1] *= -1
 
-            Third_period[flag] *= option[dart]
-
-    answer = sum(Third_period)
+    answer = sum(chances)
     return answer
 
 dartResult_1 = '1S2D*3T'
@@ -39,13 +39,44 @@ dartResult_5 = '1D#2S*3S'
 dartResult_6 = '1T2D3D#'
 dartResult_7 = '1D2S3T*'
 
-print(solution(dartResult_1))
-print(solution(dartResult_2))
-print(solution(dartResult_3))
-print(solution(dartResult_4))
-print(solution(dartResult_5))
-print(solution(dartResult_6))
-print(solution(dartResult_7))
+# print(solution(dartResult_1))
+# print(solution(dartResult_2))
+# print(solution(dartResult_3))
+# print(solution(dartResult_4))
+# print(solution(dartResult_5))
+# print(solution(dartResult_6))
+# print(solution(dartResult_7))
+
+def solution_other(dartResult):
+    answer = []
+
+    dartResult = dartResult.replace('10', 'k')
+    point = ['10' if i == 'k' else i for i in dartResult]
+
+    i = -1
+    sdt = ['S', 'D', 'T']
+    for j in point:
+        if j in sdt:
+            answer[i] = answer[i] ** (sdt.index(j) + 1)
+        elif j == '*':
+            answer[i] = answer[i] * 2
+            if i != 0:
+                answer[i - 1] = answer[i - 1] * 2
+        elif j == '#':
+            answer[i] = answer[i] * (-1)
+        else:
+            answer.append(int(j))
+            i += 1
+
+    return sum(answer)
+
+# print(solution_other(dartResult_1))
+# print(solution_other(dartResult_2))
+# print(solution_other(dartResult_3))
+# print(solution_other(dartResult_4))
+# print(solution_other(dartResult_5))
+# print(solution_other(dartResult_6))
+# print(solution_other(dartResult_7))
 
 import re
 
@@ -57,6 +88,7 @@ def solution_best(dartResult):
 
     p = re.compile('(\d+)([SDT])([*#]?)')
     dart = p.findall(dartResult)
+    print(dart)
     for i in range(len(dart)):
         if dart[i][2] == '*' and i > 0:
             dart[i - 1] *= 2
@@ -65,10 +97,10 @@ def solution_best(dartResult):
     answer = sum(dart)
     return answer
 
-print(solution_best(dartResult_1))
-print(solution_best(dartResult_2))
-print(solution_best(dartResult_3))
-print(solution_best(dartResult_4))
-print(solution_best(dartResult_5))
-print(solution_best(dartResult_6))
-print(solution_best(dartResult_7))
+# print(solution_best(dartResult_1))
+# print(solution_best(dartResult_2))
+# print(solution_best(dartResult_3))
+# print(solution_best(dartResult_4))
+# print(solution_best(dartResult_5))
+# print(solution_best(dartResult_6))
+# print(solution_best(dartResult_7))
